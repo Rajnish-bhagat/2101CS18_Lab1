07861305b377e42133e5b53b36e415f43945f6a1
Rajnish-bhagat <rajnishbhagat4@gmail.com>
2022-07-27 21:58:35 +0530

    Merge sort added

--- sorting.c
+++ sorting.c
@@ -1,5 +1,6 @@
 #include <math.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 void swap(int *xp, int *yp)
 {
@@ -7,6 +8,65 @@ void swap(int *xp, int *yp)
     *xp = *yp;
     *yp = temp;
 }
+
+
+//Merge sort
+void merge(int arr[], int l, int m, int r)
+{
+    int i, j, k;
+    int n1 = m - l + 1;
+    int n2 = r - m;
+ 
+    int L[n1], R[n2];
+ 
+    for (i = 0; i < n1; i++)
+        L[i] = arr[l + i];
+    for (j = 0; j < n2; j++)
+        R[j] = arr[m + 1 + j];
+    
+    i = 0; 
+    j = 0; 
+    k = l;
+    while (i < n1 && j < n2) {
+        if (L[i] <= R[j]) {
+            arr[k] = L[i];
+            i++;
+        }
+        else {
+            arr[k] = R[j];
+            j++;
+        }
+        k++;
+    }
+ 
+    while (i < n1) {
+        arr[k] = L[i];
+        i++;
+        k++;
+    }
+ 
+    while (j < n2) {
+        arr[k] = R[j];
+        j++;
+        k++;
+    }
+}
+ 
+void mergeSort(int arr[], int l, int r)
+{
+    if (l < r) {
+        int m = l + (r - l) / 2;
+ 
+        mergeSort(arr, l, m);
+        mergeSort(arr, m + 1, r);
+ 
+        merge(arr, l, m, r);
+    }
+}
+ 
+
+
+
 
 //Bubble sort 
 void bubbleSort(int arr[], int n)
@@ -83,7 +143,7 @@ int main()
     }
 
     int choice;
-    printf("Enter 1 for insertion sort, 2 for selection sort, 3 for bubble sort : ");
+    printf("Enter 1 for insertion sort, 2 for selection sort, 3 for bubble sort, 4 for merge sort : ");
     scanf("%d",&choice);
 
     if(choice==1)
@@ -103,6 +163,12 @@ int main()
         bubbleSort(arr, n);
         printf("The sorted array using Bubble sort algorithm is\n");
     }
+    else if(choice==4)
+    {
+        mergeSort(arr, 0, n-1);
+        printf("The sorted array using Merge sort algorithm is\n");
+    }
+    
     
     printArray(arr, n);
 
