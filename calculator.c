5f453d0d9e9f3dccfd5ce4be36e1526c3a8b074a
Rajnish-bhagat <rajnishbhagat4@gmail.com>
2022-07-27 20:15:16 +0530

    Added subtraction operation

--- calculator.c
+++ calculator.c
@@ -5,9 +5,9 @@ int main()
 
   char op;
   double first, second;
-  printf("Enter an operator (+): ");
+  printf("Enter an operator (+,-): ");
   scanf("%c", &op);
-  
+
   printf("Enter two operands: ");
   scanf("%lf %lf", &first, &second);
 
@@ -16,6 +16,9 @@ int main()
     case '+':
       printf("%.1lf + %.1lf = %.1lf", first, second, first + second);
       break;
+    case '-':
+      printf("%.1lf - %.1lf = %.1lf", first, second, first - second);
+      break;      
     
     default:
       printf("Error! operator is not correct");
