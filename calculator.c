ef2d1a1541dfeddaa6a74b6fef68d5daf0c2b4fb
Rajnish-bhagat <rajnishbhagat4@gmail.com>
2022-07-27 20:16:22 +0530

    Added multiplication operation

--- calculator.c
+++ calculator.c
@@ -5,7 +5,7 @@ int main()
 
   char op;
   double first, second;
-  printf("Enter an operator (+,-): ");
+  printf("Enter an operator (+,-,*): ");
   scanf("%c", &op);
 
   printf("Enter two operands: ");
@@ -19,7 +19,10 @@ int main()
     case '-':
       printf("%.1lf - %.1lf = %.1lf", first, second, first - second);
       break;      
-    
+    case '*':
+      printf("%.1lf * %.1lf = %.1lf", first, second, first * second);
+      break;
+
     default:
       printf("Error! operator is not correct");
   }
